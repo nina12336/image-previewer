03defec882d7b58e1d5b6e776cb92d63e1e179d7
nina <nina10313@gmail.com>
2020-12-04 15:45:59 +0800

    onchange function

--- src/ImagePreviewer.js
+++ src/ImagePreviewer.js
@@ -3,23 +3,28 @@ import './ImagePreviewer.css';
 
 
 const ImagePreviewer = () => {
+    const [url , setUrl]=React.useState ();
     return (
     <label className="wrapper" htmlFor="input">
-        <div className="description" >請選擇圖片</div>
+        { url ? 
+        <img src={url} alt="" width="400" height="200"/> :
+        <div className="description" >請選擇圖片</div>}
         <input
         type="file"
         accept="image/*"
         style={{display:"none"}}
         id="input"
-
-
-        
+        onChange ={ (e)=>{
+            const file = e.target.files[0];
+            if (file) {
+                const objectUrl = URL.createObjectURL(file);
+                setUrl(objectUrl);
+            };
+        }
+        }
         />
 
     </label>
-
     );
 };
-
-
 export default ImagePreviewer;
